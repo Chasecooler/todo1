8d02c4afc808db1d7e7d65683b760fbbd3aaa369
Chasecooler <chasemarq@yahoo.com>
2019-07-22 12:42:14 -0400

    Updates

---
+++ precache-manifest.6525f2239a014a139bf1eb6ef7f91cf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d14aee660f3ca1bb9b834d986a4d12",
+    "url": "/todo1/index.html"
+  },
+  {
+    "revision": "757a949340338f66c3cf",
+    "url": "/todo1/static/css/main.2c15803c.chunk.css"
+  },
+  {
+    "revision": "08e72e7f05dd5b0c7721",
+    "url": "/todo1/static/js/2.adf86020.chunk.js"
+  },
+  {
+    "revision": "757a949340338f66c3cf",
+    "url": "/todo1/static/js/main.e9fd099a.chunk.js"
+  },
+  {
+    "revision": "47549cd864358ec393b9",
+    "url": "/todo1/static/js/runtime~main.73b36ddd.js"
+  }
+]);
